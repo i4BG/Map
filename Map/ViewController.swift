e3afdd6c0b5fe530282232186ecc045a9170331c
Ivaylo Todorov <ivaylo.k.todorov@me.com>
2015-12-07 22:07:14 +0200

    InsertMap-iOS
    how ot insert and use Maps on iOS.
    using MapKit

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate{
 
+    @IBOutlet var map: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        var latitute:CLLocationDegrees = 40.708879
+        
+        var longitude:CLLocationDegrees = -74.011622
+        
+        var latDelta:CLLocationDegrees = 0.001
+        
+        var lonDelta:CLLocationDegrees = 0.001
+        
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitute, longitude)
+        
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        map.setRegion(region, animated: true)
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
